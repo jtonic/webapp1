a035df0390ab6183d889511113de480c12bfcdec
tony <antonel-ernest.pazargic@ing.com>
2025-07-02 20:49:11 +0300

    configure ts

---
+++ jest.config.ts
@@ -0,0 +1,6 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  transform: {},
+  extensionsToTreatAsEsm: ['.ts'],
+};
---
+++ test/math2.test.ts
@@ -0,0 +1,8 @@
+
+import { expect } from '@jest/globals';
+
+describe('TypeScript math', () => {
+  it('should assert that 2 * 2 equals 4', () => {
+    expect(2 * 2).toBe(4);
+  });
+});
